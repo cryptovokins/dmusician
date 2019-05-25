99be298a891358f0af05e43436b52551b9f338e1
Adrian Ferreres <adrian.ferreres@sngular.com>
2019-05-25 13:49:51 +0200

    First version of banner columns

---
+++ src/app/shared/components/bannerColumn/bannerColumn.component.ts
@@ -0,0 +1,39 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+import { Banner } from '../../../entities';
+@Component({
+    selector: 'banner-column',
+    templateUrl: './bannerColumn.component.html',
+    styleUrls: ['./bannerColumn.component.css']
+})
+export class BannerColumn {
+
+    @Input()
+    public banners: Banner[] = [
+        {
+            id: 0,
+            img:'/assets/test-images/mev.jpg'
+        },
+        {
+            id: 1,
+            img:'/assets/test-images/Pib.jpeg'
+        },
+        {
+            id: 2,
+            img:'/assets/test-images/tentacle.png'
+        },
+        {
+            id: 3,
+            img:'/assets/test-images/vikings.jpg'
+        }
+    ]
+
+    @Output()
+    public bannerClicked: EventEmitter<number> = new EventEmitter<number>();
+
+
+    private clickOnBanner (bannerId) {
+        this.bannerClicked.emit(bannerId);
+    }
+    
+}
