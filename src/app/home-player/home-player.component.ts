e9f1b7ae6a735acea51ce0600ca5d34aa4884c8a
Adrian Ferreres <adrian.ferreres@sngular.com>
2019-05-25 20:48:48 +0200

    Songs repo finish

--- src/app/home-player/home-player.component.ts
+++ src/app/home-player/home-player.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
-import { SessionRepoService } from '../core';
+import { SessionRepoService, SongsRepoService } from '../core';
+import { Song } from '../entities';
 
 @Component({
   selector: 'app-home-player',
@@ -9,12 +10,18 @@ import { SessionRepoService } from '../core';
 })
 export class HomePlayerComponent implements OnInit {
 
+  public songs: Song[] = [];
+  public currentSong: Song = null;
+
   constructor(
-    private sessionRepo: SessionRepoService
+    private sessionRepo: SessionRepoService,
+    private songsRepo: SongsRepoService
   ) { }
 
   ngOnInit() {
     console.log(this.sessionRepo.getId());
+    this.songs = this.songsRepo.getSongs();
+    this.currentSong = this.songsRepo.getCurrentSong();
   }
 
 }
