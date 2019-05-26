c83dbc5f7999a68ba9e53ca7b6cca76541bed392
MrCodeDev <mrcodedev@MacBook-Pro-de-Jose.local>
2019-05-26 10:54:47 +0200

    Starting get Ads

--- src/app/home-player/home-player.component.ts
+++ src/app/home-player/home-player.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ContractService } from '../util/contract.service';
 
-import { SessionRepoService, SongsRepoService, CoversRepoService } from '../core';
-import { Song, Images } from '../entities';
-import { BehaviorSubject } from 'rxjs';
-
+import { SessionRepoService, SongsRepoService } from '../core';
+import { Song } from '../entities';
+import { BannersRepoService } from '../core/repositories/banners.service';
 
 @Component({
   selector: 'app-home-player',
@@ -12,36 +11,39 @@ import { BehaviorSubject } from 'rxjs';
   styleUrls: ['./home-player.component.css']
 })
 export class HomePlayerComponent implements OnInit {
-
-
   private weisToPlay =  1000000000000000;
   private weisToClick = 5000000000000000  ;
   foundAmount = 0;
   public songs: Song[] = [];
   public currentSong: Song = null;
-  public channelCovers: BehaviorSubject<Images[]> = new BehaviorSubject<Images[]>([]);
+  public ads = [];
 
   constructor(
     private contractService:ContractService,
     private sessionRepo: SessionRepoService,
     private songsRepo: SongsRepoService,
-    private coversRepo: CoversRepoService
+    private bannerRepo: BannersRepoService
   ) { }
 
   ngOnInit() {
     console.log(this.sessionRepo.getId());
     this.songs = this.songsRepo.getSongs();
     this.currentSong = this.songsRepo.getCurrentSong();
-    this.channelCovers.next(this.coversRepo.getCovers(0,4));
   }
 
   play(){
     this.contractService.buySong(this.weisToPlay)
   }
+
   clickAdvertisment(){
     this.contractService.clickAdvertisement(this.weisToClick)
   }
+
   foundMyCompany(){
     this.contractService.foundMyContract(this.foundAmount)
+  }
+
+  getMeAd(startIndex: number, length: number) {
+    return this.bannerRepo.getBanners(startIndex, length);
   }
 }
